173169e402970b474b60e5ef5e4c4e2f067105fb
Delfx <motiejunas.lukas@gmail.com>
2023-05-18 13:57:26 +0300

    updated search UI. Deleted button

--- app/routes/components/navbar.tsx
+++ app/routes/components/navbar.tsx
@@ -29,13 +29,21 @@ export default function Navbar() {
     }
   }, [citiesFetcher.data, searchQuery]);
 
-  function handleChange(value) {
+  function handleChange(value) {    
     setSearchQuery(value);
     setFilteredCities(
       citiesFetcher.data.filter((city) =>
         city.name.toLowerCase().includes(searchQuery.toLowerCase())
       )
     );
+  }
+
+  function handleSelectCity(item) {
+    setSearchQuery(item);
+    const form = document.querySelector("Form");
+    const input = form.querySelector('input[name="cityName"]');
+    input.value = item;
+    form.submit();
   }
 
   return (
@@ -49,18 +57,25 @@ export default function Navbar() {
       <Form action="/cities?index" method="post" autoComplete="off">
         <div className="form-control">
           <div className="flex">
-            <Combobox aria-label="choose a city">
+            <Combobox aria-label="choose a city"   onSelect={(item) => handleSelectCity(item)}>
               <ComboboxInput
                 name="cityName"
                 type="text"
                 placeholder="Search For city"
                 className="input input-bordered"
                 onChange={(event) => handleChange(event.target.value)}
+                value={searchQuery}
+
               />
               <ComboboxPopover>
                 <ComboboxList>
                   {filteredCities.slice(0, 10).map((city) => (
-                    <ComboboxOption key={city.code} value={city.code}>
+                    <ComboboxOption
+                      key={city.code}
+                      value={city.code}
+                      onSelect={() => handleSelectCity(city)}
+
+                    >
                       {city.name}
                     </ComboboxOption>
                   ))}
@@ -68,22 +83,6 @@ export default function Navbar() {
               </ComboboxPopover>
             </Combobox>
 
-            <button type="submit" className="btn ml-2 btn-square">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-            </button>
           </div>
         </div>
       </Form>
