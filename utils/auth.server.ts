611031641a8658b0bb64739be0cdedd115b3305f
Lukas <motiejunas.lukas@gmail.com>
2023-06-20 15:35:07 +0300

    adding formstrategy

--- utils/auth.server.ts
+++ utils/auth.server.ts
@@ -1,15 +1,31 @@
 // app/services/auth.server.ts
-import { Authenticator } from "remix-auth";
+import { Authenticator, AuthorizationError } from "remix-auth";
 import { sessionStorage } from "./session.server";
+import { FormStrategy } from "remix-auth-form";
+
 
 
 interface User {
-    id: number;
-    username: string;
-    email: string;
-    // Add other properties specific to your user object
-  }
+  id: number;
+  username: string;
+  email: string;
+  pass: string;
+  // Add other properties specific to your user object
+}
 
-// Create an instance of the authenticator, pass a generic with what
-// strategies will return and will store in the session
 export let authenticator = new Authenticator<User>(sessionStorage);
+
+authenticator.use(
+  new FormStrategy(async ({ form }) => {
+    let email = form.get("email");
+    let password = form.get("pass");
+    let user = await login(email, password);
+    // the type of this user must match the type you pass to the Authenticator
+    // the strategy will automatically inherit the type if you instantiate
+    // directly inside the `use` method
+    return user;
+  }),
+  // each strategy has a name and can be changed to use another one
+  // same strategy multiple times, especially useful for the OAuth2 strategy.
+  "user-pass"
+);
